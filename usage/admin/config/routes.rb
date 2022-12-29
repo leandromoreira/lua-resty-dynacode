43cf50eb357dcdfae85f795afe3fa5824d4fe706
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2022-12-29 14:34:19 -0300

    fix admin root index

--- usage/admin/config/routes.rb
+++ usage/admin/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'plugins/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: "admin/computing_units#index"
+  root to: "admin/plugins#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
